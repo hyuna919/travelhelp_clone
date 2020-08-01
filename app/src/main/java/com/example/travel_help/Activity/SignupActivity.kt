6426ad8df87604fea9f8aad52bdb82308c4d5ba7
hyuna919 <hyuna919@naver.com>
2020-08-02 02:09:06 +0900

    회원가입-아이디 중복, 비번 불일치시 붉은글씨로 경고 나올 수 있게 xml수정

--- app/src/main/java/com/example/travel_help/Activity/SignupActivity.kt
+++ app/src/main/java/com/example/travel_help/Activity/SignupActivity.kt
@@ -5,17 +5,36 @@ import androidx.appcompat.app.AppCompatActivity
 import android.view.View
 import android.widget.Toolbar
 import com.example.travel_help.R
+import kotlinx.android.synthetic.main.signup.*
 
 
 class SignupActivity : AppCompatActivity() {
+    var checkId = false
+    var checkPw = false
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.signup)
 
-        val tb = findViewById<View>(R.id.board_tb) as Toolbar
+        bt_check.setOnClickListener(View.OnClickListener {
+            var id = bt_check.text.toString()
+
+            checkId(id)
+        })
+
+        //val tb = findViewById<View>(R.id.board_tb) as Toolbar
         //setSupportActionBar(tb)
+
+    }
+
+    fun checkId(id:String){
+        var idList = intent.getStringArrayListExtra("id")
+        for(list in idList){
+            if(list==id){
+
+            }
+        }
 
     }
 }
