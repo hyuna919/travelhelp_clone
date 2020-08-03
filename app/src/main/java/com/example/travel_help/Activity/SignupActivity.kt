6f6432e1fd6661fa6f00d5f9fed212f7cdfa8e15
hyuna919 <hyuna919@naver.com>
2020-08-03 16:41:18 +0900

    회원가입-비밀번호 확인

--- app/src/main/java/com/example/travel_help/Activity/SignupActivity.kt
+++ app/src/main/java/com/example/travel_help/Activity/SignupActivity.kt
@@ -2,6 +2,8 @@ package com.example.travel_help.Activity
 
 import android.graphics.Color
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import android.view.View
@@ -21,8 +23,23 @@ class SignupActivity : AppCompatActivity() {
 
         bt_check.setOnClickListener(View.OnClickListener {
             var id = put_id.text.toString()
-            Log.d("------------","1111111111")
             checkId(id)
+        })
+
+        put_pw_check.addTextChangedListener(object: TextWatcher {
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+                var pw = put_pw.text.toString()
+                var check_pw = put_pw_check.text.toString()
+                checkPw(pw,check_pw)
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+
         })
 
         //val tb = findViewById<View>(R.id.board_tb) as Toolbar
@@ -45,15 +62,23 @@ class SignupActivity : AppCompatActivity() {
         }
 
         if(!notUniq){
-
-            Log.d("------------","0000000000")
             checkId = true
             notUniq = false
             tv_idcheck.setText("사용 가능한 아이디입니다.")
             tv_idcheck.setTextColor(Color.GREEN)
         }
+    }
 
 
-
+    fun checkPw(pw1:String, pw2:String){
+        if(pw1==pw2){
+            tv_pwcheck.setText("비밀번호가 일치합니다.")
+            tv_pwcheck.setTextColor(Color.GREEN)
+            checkPw = true
+        }else{
+            tv_pwcheck.setText("비밀번호가 일치하지 않습니다")
+            tv_pwcheck.setTextColor(Color.RED)
+            checkPw = false
+        }
     }
 }
