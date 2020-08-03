2483569640d90d52dc4f9f86f7d7dc491a55044a
hyuna919 <hyuna919@naver.com>
2020-08-03 17:09:36 +0900

    회원가입-아이디중복확인 수정

--- app/src/main/java/com/example/travel_help/Activity/SignupActivity.kt
+++ app/src/main/java/com/example/travel_help/Activity/SignupActivity.kt
@@ -7,6 +7,7 @@ import android.text.TextWatcher
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import android.view.View
+import android.widget.Toast
 import android.widget.Toolbar
 import com.example.travel_help.R
 import kotlinx.android.synthetic.main.signup.*
@@ -14,18 +15,20 @@ import kotlinx.android.synthetic.main.signup.*
 
 class SignupActivity : AppCompatActivity() {
     var checkId = false
-    var notUniq = false
+    var uniqId = false
     var checkPw = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.signup)
 
+        //아이디 중복 확인
         bt_check.setOnClickListener(View.OnClickListener {
             var id = put_id.text.toString()
             checkId(id)
         })
 
+        //비밀번호 확인
         put_pw_check.addTextChangedListener(object: TextWatcher {
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
 
@@ -39,7 +42,22 @@ class SignupActivity : AppCompatActivity() {
 
             override fun afterTextChanged(p0: Editable?) {
             }
+        })
 
+        btn_finish.setOnClickListener(View.OnClickListener{
+            if(checkId){
+                if(uniqId){
+                    if(checkPw){
+                        //완료 인텐트
+                    }else{
+                        Toast.makeText(applicationContext, "비밀번호를 확인해주세요.", Toast.LENGTH_LONG).show()
+                    }
+                }else{
+                    Toast.makeText(applicationContext, "아이디를 변경해주세요.", Toast.LENGTH_LONG).show()
+                }
+            }else{
+                Toast.makeText(applicationContext, "아이디 중복확인 버튼을 눌러주세요.", Toast.LENGTH_LONG).show()
+            }
         })
 
         //val tb = findViewById<View>(R.id.board_tb) as Toolbar
@@ -48,22 +66,23 @@ class SignupActivity : AppCompatActivity() {
     }
 
     fun checkId(id:String){
-        notUniq = false
+        var uniq = true
         var idList = intent.getStringArrayExtra("id")
 
         for(list in idList){
             if(list==id){
+                Log.d("-----------------------","ddddddddddddd")
                 put_id.setText(null)
                 tv_idcheck.setText("이미 존재하는 아이디입니다.")
                 tv_idcheck.setTextColor(Color.RED)
-                notUniq = true
+                uniq = false
                 break
             }
         }
 
-        if(!notUniq){
+        if(uniq){
             checkId = true
-            notUniq = false
+            uniqId = true
             tv_idcheck.setText("사용 가능한 아이디입니다.")
             tv_idcheck.setTextColor(Color.GREEN)
         }
